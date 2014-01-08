4abdfd0771e8821edcc2b86b5ef02e61828a4be9
Seiei Higa <sei@Kernel.local>
2014-01-08 21:46:19 +0900

    Install Google Japanese Input

--- modules/people/manifests/hanachin.pp
+++ modules/people/manifests/hanachin.pp
@@ -2,5 +2,12 @@ class people::hanachin {
   include chrome
   include dropbox
   include vagrant
+
+  package {
+    'GoogleJapaneseInput':
+      source => "http://dl.google.com/japanese-ime/latest/GoogleJapaneseInput.dmg",
+      provider => pkgdmg;
+  }
+
   $home = "/Users/${::boxen_user}"
 }
