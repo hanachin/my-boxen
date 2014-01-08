d445384e2865e99c6bb13f8e88f0a5854e2a5d58
Seiei Higa <sei@Kernel.local>
2014-01-09 02:53:10 +0900

    Specify global ruby version, 2.1.0

--- manifests/site.pp
+++ manifests/site.pp
@@ -75,6 +75,10 @@ node default {
   ruby::version { '2.0.0': }
   ruby::version { '2.1.0': }
 
+  class { 'ruby::global':
+    version => '2.1.0'
+  }
+
   # common, useful packages
   package {
     [
