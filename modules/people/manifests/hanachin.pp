7b83615e702680118cf951917954d7af971c5ea0
Seiei Higa <sei@Kernel.local>
2014-01-08 22:01:01 +0900

    Install Postgres93

--- modules/people/manifests/hanachin.pp
+++ modules/people/manifests/hanachin.pp
@@ -7,6 +7,9 @@ class people::hanachin {
     'GoogleJapaneseInput':
       source => "http://dl.google.com/japanese-ime/latest/GoogleJapaneseInput.dmg",
       provider => pkgdmg;
+    'Postgresapp':
+      source => 'https://github.com/PostgresApp/PostgresApp/releases/download/9.3.1.0-alpha1/Postgres93.zip',
+      provider => 'compressed_app';
   }
 
   $home = "/Users/${::boxen_user}"
