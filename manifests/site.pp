d9b82bf6389bf2d328d33de8880cf54454afeca8
Seiei Higa <hanachin@gmail.com>
2014-05-12 21:50:53 +0900

    Install ruby 2.1.2 && 2.0.0-p481

--- manifests/site.pp
+++ manifests/site.pp
@@ -72,9 +72,12 @@ node default {
   ruby::version { '1.8.7': }
   ruby::version { '1.9.2': }
   ruby::version { '1.9.3': }
+  ruby::version { '2.0.0-p247': }
+  ruby::version { '2.0.0-p481': }
   ruby::version { '2.0.0': }
   ruby::version { '2.1.0': }
   ruby::version { '2.1.1': }
+  ruby::version { '2.1.2': }
 
   class { 'ruby::global':
     version => '2.1.1'
